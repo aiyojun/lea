bd952dc331cafef8cd7b7a22b7282b9a5dcb54b4
aiyo <1608450902@qq.com>
2021-09-03 15:04:10 +0800

    nothing

--- src/AST/build.sh
+++ src/AST/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p test && cp *.cpp *.h ../../grammar/comment.l ../../grammar/syntax.l ../../grammar/syntax.y ../basic_ds.* test && cd test \
+mkdir -p test && cp *.cpp *.h ../../leapro/main.lea ../../grammar/comment.l ../../grammar/syntax.l ../../grammar/syntax.y ../basic_ds.* test && cd test \
 && flex -+ -P mm -o comment.cc comment.l \
 && bison -L C++ -v -d syntax.y \
 && flex -+ syntax.l \
